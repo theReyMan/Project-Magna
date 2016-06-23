86d6d5d5f80f8a5dfba2d10f00623e4e4d98f96e
theReyMan <rey@jolton.com>
2016-06-23 16:20:04 -0500

    Pointer Creation

---
+++ Assets/PointerRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerRotate : MonoBehaviour
+{
+	public int speed = 20;
+
+	void Start ()
+	{
+		//playerLoc = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.RotateAround (transform.position, Vector3.up, speed * Time.deltaTime);
+	}
+}
