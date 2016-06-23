2fab646606ee469b4cc57ded1f11234cac6de1e2
theReyMan <rey@jolton.com>
2016-06-22 19:19:17 -0500

    Initial Commit
    All initial assets to begin development

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+	public int speed = 10;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		if (Input.GetKey ("up")) {
+			this.GetComponent<Rigidbody> ().AddForce (Vector3.forward * speed);
+		}
+			
+		if (Input.GetKey ("right")) {
+			this.GetComponent<Rigidbody> ().AddForce (Vector3.right * speed);
+
+		}
+			
+		if (Input.GetKey ("down")) {
+			this.GetComponent<Rigidbody> ().AddForce (Vector3.back * speed);
+		}
+			
+		if (Input.GetKey ("left")) {
+			this.GetComponent<Rigidbody> ().AddForce (Vector3.left * speed);
+		}
+			
+
+	}
+
+}
